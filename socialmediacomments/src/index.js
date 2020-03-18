b3a9011fc97f43ed70866d6bba4bc9115370b446
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-18 12:55:03 +0000

    Use props for post time and content

--- socialmediacomments/src/index.js
+++ socialmediacomments/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+// import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail author="Sam" timeAgo="Today at 1:00pm" content="Hello everyone!" />
+      <CommentDetail author="Alex" timeAgo="Yesterday at 8:00pm" content="Wonderful" />
+      <CommentDetail author="Jane" timeAgo="Monday at 2:00pm" content="I'm learning React" />
     </div>
   )
 };
