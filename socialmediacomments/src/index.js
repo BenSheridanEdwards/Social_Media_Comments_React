5b07c9d4e8e91cd0d769a524a4e5ca9e227e2d93
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-18 14:24:38 +0000

    Add ApprovalCard to React App

--- socialmediacomments/src/index.js
+++ socialmediacomments/src/index.js
@@ -2,28 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 1:00pm" 
-        content="Hello everyone!" 
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail 
-        author="Alex" 
-        timeAgo="Yesterday at 8:00pm" 
-        content="Wonderful" 
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail 
-        author="Jane" 
-        timeAgo="Monday at 2:00pm" 
-        content="I'm learning React" 
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 1:00pm" 
+          content="Hello everyone!" 
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Yesterday at 8:00pm" 
+          content="Wonderful" 
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Monday at 2:00pm" 
+          content="I'm learning React" 
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   )
 };
